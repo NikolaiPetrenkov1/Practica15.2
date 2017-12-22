c67627e165343b42290c2efad653387b01b48d2b
Петренков Николай <@corp.itstep>
2017-12-22 09:06:24 +0600

    ttt

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string path = @"C:\d\ff.txt";
+            using (StreamWriter stream = new StreamWriter(path))
+            {
+                stream.WriteLine("Nikolai");
+                stream.WriteLine("Petrenkov");
+                stream.WriteLine("17");
+            }
+        }
+    }
+}
